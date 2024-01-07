ff90f854af7c72ee59350faf0504c471ef716c4b
ashalfarhan <ashalfarhan8@gmail.com>
2024-01-07 16:43:15 +0700

    chore: format

--- src/routes/[planet]/+page.ts
+++ src/routes/[planet]/+page.ts
@@ -3,11 +3,11 @@ import type { PageLoad } from './$types';
 import planets from '$lib/data.json';
 
 export const load: PageLoad = ({ params }) => {
-  const planet = planets.find((p) => p.name.toLowerCase() === params.planet.toLowerCase());
+  const planet = planets.find(p => p.name.toLowerCase() === params.planet.toLowerCase());
   if (!planet) return redirect(303, '/earth');
   return {
     planet,
-    planetLinks: planets.map((p) => ({
+    planetLinks: planets.map(p => ({
       name: p.name,
       accent_color: p.accent_color,
     })),
