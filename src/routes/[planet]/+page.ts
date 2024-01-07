e12948aa2b3437a49e2295c817634c388635b992
ashalfarhan <ashalfarhan8@gmail.com>
2024-01-07 16:54:10 +0700

    fix: remove unused page data

--- src/routes/[planet]/+page.ts
+++ src/routes/[planet]/+page.ts
@@ -5,11 +5,5 @@ import planets from '$lib/data.json';
 export const load: PageLoad = ({ params }) => {
   const planet = planets.find(p => p.name.toLowerCase() === params.planet.toLowerCase());
   if (!planet) return redirect(303, '/earth');
-  return {
-    planet,
-    planetLinks: planets.map(p => ({
-      name: p.name,
-      accent_color: p.accent_color,
-    })),
-  };
+  return { planet };
 };
